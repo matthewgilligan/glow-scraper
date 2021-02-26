599cae045191f9875ed6b797e2f1785bfc54b94e
Matthew Gilligan <mcgilligan97@gmail.com>
2021-02-26 21:36:11 +0000

    Initial commit

---
+++ glow-scraper/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace MusicScraper
+{
+    class Program
+    {
+        static ScrapingBrowser _scrapingBrowser = new ScrapingBrowser();
+
+        static void Main(string[] args)
+        {
+            var url = "https://rateyourmusic.com/artist/fishmans";
+
+            GetArtistDetails(url);
+        }
+
+        static ArtistDetails GetArtistDetails(string url)
+        {
+            var htmlNode = GetHtml(url);
+            var artistDetails = new ArtistDetails();
+
+            var NameNode = htmlNode.CssSelect(".artist_name_hdr").First();
+            artistDetails.Name = NameNode.InnerText;
+
+            return artistDetails;
+        }
+
+        static HtmlNode GetHtml(string url)
+        {
+            var webpage = _scrapingBrowser.NavigateToPage(new Uri(url));
+            return webpage.Html;
+        }
+    }
+
+    public class ArtistDetails
+    {
+        public string Name { get; set; }
+        public string Formed { get; set; }
+    }
+}
